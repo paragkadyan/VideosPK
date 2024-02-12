c730bfed50dacb314a0c73de8eaa16f2a26c4d89
Parag <kadyanparag@gmail.com>
2024-02-12 13:58:06 +0530

    register user code

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -82,7 +82,7 @@ userSchema.methods.generateAcessToken = function(){
     expiresIn:process.env.ACESS_TOKEN_EXPIRY
     })
 }
-generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign({
     _id:this._id
 },
